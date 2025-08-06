4fd4495e08ecbf2850e77a7a830e488924405d0b
Byagari-Sai-Kumar <sai815538@gmail.com>
2025-08-06 22:33:49 +0530

    functions and console in JS

---
+++ variables/7functions.js
@@ -0,0 +1,31 @@
+//Functions are created to made code separation where huge things are working together
+//function keyword is used whenever creating a function with
+//function keyword
+//function name
+//curly braces()
+//flower braces{}
+//return value
+
+function fun(){
+    var a = 1 + 2;
+    console.log(a);
+    //alert('Hello from fun function!!');
+}
+
+//function's code will be only executed when the function is called or invocked
+fun();
+
+//we can call function any number of times
+fun();
+fun();
+fun();
+
+//function can take parameters or arguments
+//here name is the parameter
+//functions can return the values or omitted is not needed.
+function greet(name){
+    return "Hello " + name + "!!";
+}
+
+const greetOne = greet('Sai Kumar');
+console.log(greetOne);
